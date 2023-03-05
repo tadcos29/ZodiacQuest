82bf7e849ef0383bc583e703add581cf000777e7
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-03-05 14:12:05 -0500

    Small hotfix for homepage.

--- public/js/game/gameinit.js
+++ public/js/game/gameinit.js
@@ -5,7 +5,11 @@ function runGame() {
 
     let game = new Phaser.Game ( {
         width: 500, height: 340,
-        scale: {mode: Phaser.Scale.FIT},
+        scale: {mode: Phaser.Scale.FIT,
+            max: {
+                width: 800,
+                height: 600
+            }},
         backgroundColor: '#3498db',
         physics: {default: 'arcade'},
         parent: "parent"
