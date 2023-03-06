9c6045e057cfbe747bc2cbe76275a1afd6bbae0e
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-03-05 19:32:44 -0500

    Icon selection

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,5 @@
+
+
 var elementSelected = null;
 var typeSelected = false;
 
@@ -18,15 +20,32 @@ $(document).on('input', '#text-src', function(){
   typeSelected = true;
 })
 
-$(document).on('click', '#picker-confirm', function(){
+$(document).on('click', '#picker-confirm', async function(){
   $('.select-image').hide();
   if(typeSelected == true){
     $('.see-choice > img').attr('src', elementSelected.val());
+ 
+   
+
   }
   else{
     $('.see-choice > img').attr('src', elementSelected.attr('src'));
   }
   $('.see-choice').fadeIn('high');
+  try {
+    const response = await fetch('/api/gamedata/skin', {
+      method: 'PUT',
+      body: JSON.stringify({
+        image: $('.see-choice > img').attr('src')
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+        } catch (error) {
+    console.error(error)
+  }
+
 })
 
 $(document).on('click', '#picker-other', function(){
